548552a3ad84c85d8d203f6860da738d213925a5
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-07-16 20:17:33 -0400

    Connected to MongoDB

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ let MongoStore = require('connect-mongo')(session);
 
 require('dotenv').config();
 
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true} )
-//     .then( ()=> {
-//         console.log('MONGODB CONNECTED')
-//     })
-//     .catch( err => console.log(`ERROR: ${err}`))
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true} )
+    .then( ()=> {
+        console.log('MONGODB CONNECTED')
+    })
+    .catch( err => console.log(`ERROR: ${err}`))
 
 let app = express();
 
@@ -36,9 +36,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
-// app.use(session());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({ url: process.env.MONGODB_URI, autoReconnect: true}),
+    cookie: {
+        secure: false,
+        maxAge: 365 * 24 * 60 * 60 * 1000
+    }
+}));
 
 app.use(flash());
 app.use(passport.initialize());
@@ -46,15 +55,15 @@ app.use(passport.session());
 
 // require('./lib/passport/passport')(passport);
 
-// app.use(function(req, res, next) {
-//     res.locals.user         =   req.user;
+app.use(function(req, res, next) {
+    res.locals.user         =   req.user;
 
-//     res.locals.error        =   req.flash("error");
-//     res.locals.error_msg    =   req.flash("error_msg");
-//     res.locals.success_msg  =   req.flash("success_msg");
+    res.locals.error        =   req.flash("error");
+    res.locals.error_msg    =   req.flash("error_msg");
+    res.locals.success_msg  =   req.flash("success_msg");
 
-//     next();
-// });
+    next();
+});
 
 // app.use(expressValidator({
 //     errorFormatter: function(param, message, value) {
