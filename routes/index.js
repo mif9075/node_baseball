706c1ec7208df12fbb6d6c66e83f3c0aaa29f29c
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-07-23 13:06:56 -0400

    Created routes for scores, standings, tickets, scores and teams in index.js

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,27 @@ router.get('/', function (req, res) {
 });
 
 router.get('/scores', function (req, res) {
-    res.render('scores/scores.ejs')
+    res.render('index/scores.ejs')
 });
+
+router.get('/standings', function (req, res) {
+    res.render('index/standings.ejs')
+});
+
+router.get('/tickets', function (req, res) {
+    res.render('index/tickets.ejs')
+});
+
+router.get('/scores', function (req, res) {
+    res.render('index/scores.ejs')
+});
+
+router.get('/teams', function (req, res) {
+    res.render('index/teams.ejs')
+});
+
+
+
 
 module.exports = router;
 
