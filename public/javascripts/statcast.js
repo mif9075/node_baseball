c48c650ee0cc493492c6926398e4c602bd5db8f5
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-07-24 18:02:28 -0400

    Added Statcast HR Leaders 2015-2019

--- public/javascripts/statcast.js
+++ public/javascripts/statcast.js
@@ -1,139 +1,146 @@
-//Adding leading zeroes to day and month
-window.onload = function () {
-    init()
+window.onload = init;
+
+function init() {
+    document.querySelector('#option2019')
+        .addEventListener('click', buttonRequest);
+    document.querySelector('#option2018')
+        .addEventListener('click', buttonRequest);
+    document.querySelector('#option2017')
+        .addEventListener('click', buttonRequest);
+    document.querySelector('#option2016')
+        .addEventListener('click', buttonRequest);
+    document.querySelector('#option2015')
+        .addEventListener('click', buttonRequest);
 }
 
-function init(){
+function buttonRequest(event){
+    let num = event.target.innerText;
+    sendRequest(num);
+}
+function sendRequest(num) {
+
+    const xhr = new XMLHttpRequest();
+    let url = `http://lookup-service-prod.mlb.com/json/named.psc_leader_hit_hr_dist.bam?season=${num}&game_type=%27D%27&game_type=%27L%27&game_type%27W%27&game_type=%27F%27&min_hip_count=15`;
+
+    xhr.open('GET', url);
+    xhr.onload = handleData;
+    xhr.send();
+
+    console.log(url)
+}
+
+function handleData(event) {
+    const person = JSON.parse(event.target.responseText);
+    displayTable(person);
+}
+
+function displayTable(player){
+
+    clearList();
+
+    if (player.psc_leader_hit_hr_dist.queryResults.totalSize === "0") {
+        const noDataTD = document.querySelector('.no_data');
+        const noDataData = document.createElement('p');
+        noDataData.innerText = "No data for this game type/year combination";
+        noDataTD.appendChild(noDataData);
+
+        }else{
+
+        for (let i = 0; i < player.psc_leader_hit_hr_dist.queryResults.row.length; i++) {
+
+        const batterNumTD = document.querySelector('.batter');
+        const batterNumData = document.createElement('p');
+        batterNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].batter;
+        batterNumTD.appendChild(batterNumData);
+
+        const distanceNumTD = document.querySelector('.distance');
+        const distanceNumData = document.createElement('p');
+        distanceNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].distance;
+        distanceNumTD.appendChild(distanceNumData);
+
+        const exitNumTD = document.querySelector('.launch_speed');
+        const exitNumData = document.createElement('p');
+        exitNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].launch_speed;
+        exitNumTD.appendChild(exitNumData);
+
+        const launchNumTD = document.querySelector('.launch_angle');
+        const launchNumData = document.createElement('p');
+        launchNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].launch_angle;
+        launchNumTD.appendChild(launchNumData);
+
+        const heightNumTD = document.querySelector('.height');
+        const heightNumData = document.createElement('p');
+        heightNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].height;
+        heightNumTD.appendChild(heightNumData);
+
+        const pitcherNumTD = document.querySelector('.pitcher');
+        const pitcherNumData = document.createElement('p');
+        pitcherNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].pitcher;
+        pitcherNumTD.appendChild(pitcherNumData);
+
+        const pitchNumTD = document.querySelector('.pitch_speed');
+        const pitchNumData = document.createElement('p');
+        pitchNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].pitch_speed;
+        pitchNumTD.appendChild(pitchNumData);
+
+        const dateNumTD = document.querySelector('.game_date');
+        const dateNumData = document.createElement('p');
+        dateNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].game_date;
+        dateNumTD.appendChild(dateNumData);
+
+        }
+    }
+}
+
+function clearList() {
+    event.preventDefault();
+    removeAllChildrenOfOl();
+}
+
+function removeAllChildrenOfOl() {
+    const ol = document.querySelector('.batter');
+    while (ol.hasChildNodes()) {
+        ol.removeChild(ol.firstChild);
+    }
+
+    const ol1 = document.querySelector('.distance');
+    while (ol1.hasChildNodes()) {
+        ol1.removeChild(ol1.firstChild);
+    }
     
-    let httpRequest
-
-    function makeRequest() {
-        httpRequest = new XMLHttpRequest()
-
-        if(!httpRequest) {
-            alert("Giving up! Cannot create an XMLHTTP instance")
-        }
-
-        httpRequest.onreadystatechange = processContents
-        httpRequest.open("GET", 'https://erikberg.com/mlb/standings.json'
-        )
-        httpRequest.send()
-        console.log(` `, httpRequest);
-        
-        
-
+    const ol2 = document.querySelector('.launch_speed');
+    while (ol2.hasChildNodes()) {
+        ol2.removeChild(ol2.firstChild);
     }
 
-
-    function processContents() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                let data = httpRequest.responseText
-                console.log(data);
-                
-                if (data) {
-                    data = JSON.parse(data)
-                    // console.log(` `, data.data.games.game);
-
-                    if (data.data.games.game) createCards(data.data.games.game)
-                    
-                }
-            } else {
-                alert("There was a problem with request")
-            }
-        }
+    const ol3 = document.querySelector('.launch_angle');
+    while (ol3.hasChildNodes()) {
+        ol3.removeChild(ol3.firstChild);
     }
 
-
-    function createCards(items) {
-        let cardDeck = document.querySelector(".container-standing > .content-standing")
-        // console.log(items);
-
-        let cards = ``
-
-        
-
-        for (let item in items) {
-            if (items.hasOwnProperty(item)) {
-                if ((items[item].status.status === 'Preview') || (items[item].status.status === 'Pre-Game')) {
-                // console.log(items[item].status.status);
-                cards += `<div class="col-md-4">
-                <div class="card-header">
-                <p class="my-0 font-weight-normal">${items[item].time} ${items[item].ampm} ${items[item].time_zone}</p>
-                </div>
-                <div class="card-body">
-                    
-                    <h6>${items[item].away_team_name}</br>${items[item].away_win}-${items[item].away_loss}</h6>
-                    <h6>${items[item].home_team_name}</br>${items[item].home_win}-${items[item].home_loss}</h6>
-                    <hr>
-                    <h6>${items[item].away_name_abbrev}: ${items[item].away_probable_pitcher.last}, ${items[item].away_probable_pitcher.first}
-                    
-                    ${items[item].away_probable_pitcher.s_wins}-${items[item].away_probable_pitcher.s_losses}, ${items[item].away_probable_pitcher.s_era} ERA
-                    </h6>
-                    <h6>${items[item].home_name_abbrev}: ${items[item].home_probable_pitcher.last}, ${items[item].home_probable_pitcher.first}
-        
-                    ${items[item].home_probable_pitcher.s_wins}-${items[item].home_probable_pitcher.s_losses}, ${items[item].home_probable_pitcher.s_era} ERA
-                    </h6>
-                    <p>At 
-                    ${items[item].venue}
-                    </p>
-                    
-                  </div>
-                </div>
-              </div>`
-                } else if (items[item].status.status === 'Final') {
-
-                cards += `<div class="col-md-4">
-                <div class="card-header">
-                <p class="my-0 font-weight-normal">${items[item].time} ${items[item].ampm} ${items[item].time_zone}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SCORE
-                </p>
-                </div>
-                <div class="card-body">
-                    
-                    <h6>${items[item].away_team_name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${items[item].linescore.r.away}</br>${items[item].away_win}-${items[item].away_loss}</h6>
-                    <h6>${items[item].home_team_name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${items[item].linescore.r.home}</br>${items[item].home_win}-${items[item].home_loss}</h6>
-                    <hr>
-                    <h6>W: ${items[item].winning_pitcher.last}, ${items[item].losing_pitcher.first}
-                    
-                    ${items[item].winning_pitcher.wins}-${items[item].winning_pitcher.losses}, ${items[item].winning_pitcher.era} ERA
-                    </h6>
-                    <h6>L: ${items[item].losing_pitcher.last}, ${items[item].losing_pitcher.first}
-        
-                    ${items[item].losing_pitcher.wins}-${items[item].losing_pitcher.losses}, ${items[item].losing_pitcher.era} ERA
-                    </h6>
-                    <p>At 
-                    ${items[item].venue}
-                    </p>
-                    
-                  </div>
-                </div>
-              </div>`
-
-                } else {
-                    cards += `<div class="col-md-4">
-                <div class="card-header">
-                <p class="my-0 font-weight-normal">${items[item].time} ${items[item].ampm} ${items[item].time_zone}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SCORE</p>
-                </div>
-                <div class="card-body">
-                    
-                    <h6>${items[item].away_team_name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${items[item].linescore.r.away}</br>${items[item].away_win}-${items[item].away_loss}</h6>
-                    <h6>${items[item].home_team_name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${items[item].linescore.r.home}</br>${items[item].home_win}-${items[item].home_loss}</h6>
-                    <hr>
-                    
-                    <p>At 
-                    ${items[item].venue}
-                    </p>
-                    
-                  </div>
-                </div>
-              </div>`
-                }
-            }
-        }
-        cardDeck.innerHTML = cards;
-        // console.log(cards)
+    const ol4 = document.querySelector('.height');
+    while (ol4.hasChildNodes()) {
+        ol4.removeChild(ol4.firstChild);
     }
 
-    makeRequest();
-    
+    const ol5 = document.querySelector('.pitcher');
+    while (ol5.hasChildNodes()) {
+        ol5.removeChild(ol5.firstChild);
+    }
+
+    const ol6 = document.querySelector('.pitch_speed');
+    while (ol6.hasChildNodes()) {
+        ol6.removeChild(ol6.firstChild);
+    }
+
+    const ol7 = document.querySelector('.game_date');
+    while (ol7.hasChildNodes()) {
+        ol7.removeChild(ol7.firstChild);
+    }
+
+    const ol8 = document.querySelector('.no_data');
+    while (ol8.hasChildNodes()) {
+        ol8.removeChild(ol8.firstChild);
+    }
+
 }
