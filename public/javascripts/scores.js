7100610ec2481a420bda4f5bb8d9d76c84df4990
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-07-24 12:16:12 -0400

    Added status to scores.js

--- public/javascripts/scores.js
+++ public/javascripts/scores.js
@@ -85,16 +85,18 @@ function init(){
     }
 
 
-
     function createCards(items) {
         let cardDeck = document.querySelector(".container > .content")
         // console.log(items);
 
         let cards = ``
 
+        
+
         for (let item in items) {
             if (items.hasOwnProperty(item)) {
-                // console.log(items[item].time);
+                if ((items[item].status.status === 'Preview') || (items[item].status.status === 'Warmup') || (items[item].status.status === 'Pre-Game')) {
+                // console.log(items[item].status.status);
                 cards += `<div class="col-md-4">
                 <div class="card-header">
                 <p class="my-0 font-weight-normal">${items[item].time} ${items[item].ampm} ${items[item].time_zone}</p>
@@ -108,7 +110,7 @@ function init(){
                     
                     ${items[item].away_probable_pitcher.s_wins}-${items[item].away_probable_pitcher.s_losses}, ${items[item].away_probable_pitcher.s_era} ERA
                     </h6>
-                    <h6>${items[item].home_name_abbrev}: ${items[item].home_probable_pitcher.last}${items[item].home_probable_pitcher.first}
+                    <h6>${items[item].home_name_abbrev}: ${items[item].home_probable_pitcher.last}, ${items[item].home_probable_pitcher.first}
         
                     ${items[item].home_probable_pitcher.s_wins}-${items[item].home_probable_pitcher.s_losses}, ${items[item].home_probable_pitcher.s_era} ERA
                     </h6>
@@ -119,6 +121,34 @@ function init(){
                   </div>
                 </div>
               </div>`
+                } else if (items[item].status.status === 'Final') {
+
+                    cards += `<div class="col-md-4">
+                <div class="card-header">
+                <p class="my-0 font-weight-normal">${items[item].time} ${items[item].ampm} ${items[item].time_zone}</p>
+                </div>
+                <div class="card-body">
+                    
+                    <h6>${items[item].away_team_name}</br>${items[item].away_win}-${items[item].away_loss}</h6>
+                    <h6>${items[item].home_team_name}</br>${items[item].home_win}-${items[item].home_loss}</h6>
+                    <hr>
+                    <h6>W: ${items[item].winning_pitcher.last}, ${items[item].losing_pitcher.first}
+                    
+                    ${items[item].winning_pitcher.wins}-${items[item].winning_pitcher.losses}, ${items[item].winning_pitcher.era} ERA
+                    </h6>
+                    <h6>L: ${items[item].losing_pitcher.last}, ${items[item].losing_pitcher.first}
+        
+                    ${items[item].losing_pitcher.wins}-${items[item].losing_pitcher.losses}, ${items[item].losing_pitcher.era} ERA
+                    </h6>
+                    <p>At 
+                    ${items[item].venue}
+                    </p>
+                    
+                  </div>
+                </div>
+              </div>`
+
+                }
             }
         }
         cardDeck.innerHTML = cards;
