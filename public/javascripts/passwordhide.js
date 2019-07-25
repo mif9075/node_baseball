90bab9d08b19381a7d606e46a8bca5290a673150
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-07-25 12:28:55 -0400

    Show Password in signin and signup

---
+++ public/javascripts/passwordhide.js
@@ -0,0 +1,8 @@
+function passwordHide() {
+    var x = document.getElementById("inputPassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
